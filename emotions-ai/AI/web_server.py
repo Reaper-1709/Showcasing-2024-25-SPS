9e986f28ddd8cf6c784059f036ab19a2eb0f5e19
dharmik2319 <126943942+dharmik2319@users.noreply.github.com>
2024-05-20 20:11:53 +0530

    added some flask foundation

---
+++ emotions-ai/AI/web_server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, g
+
+app: Flask = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    g.emoji = 'hello'
+    return render_template('index.html')
+
+@app.route('/set_emoji', methods=['POST', 'GET'])
+def get_emoji():
+    g.emoji = request.args.get('emoji')
+    set_emoji()
+    return g.emoji
+
+@app.context_processor
+def set_emoji():
+    return {'emoji': g.emoji}
+
+
+if __name__=="__main__":
+    app.run(port=8000, debug=True)
