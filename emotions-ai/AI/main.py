09f101c5011eedc0edf743280cd02ce38a04068c
Dharmik Kaku <126943942+dharmik2319@users.noreply.github.com>
2024-05-29 16:37:41 +0530

    ran autopep8 on main.py

--- emotions-ai/AI/main.py
+++ emotions-ai/AI/main.py
@@ -3,10 +3,10 @@ import cv2
 import time
 import requests
 session = requests.Session()
-feed = cv2.VideoCapture(0) #0 --> Default webcam
-detector = FER(mtcnn=True) #if set to false, it uses haarcascade
+feed = cv2.VideoCapture(0)  # 0 --> Default webcam
+detector = FER(mtcnn=True)  # if set to false, it uses haarcascade
 
-emo_map = {"neutral": "😑", #maps an emoji to the emotions
+emo_map = {"neutral": "😑",  # maps an emoji to the emotions
            "happy": "😀",
            "sad": "😔",
            "angry": "😡",
@@ -14,16 +14,16 @@ emo_map = {"neutral": "😑", #maps an emoji to the emotions
            "disgust": "🤢",
            "fear": "😨"}
 emo_grammar = {"neutral": "neutral",
-        "happy": "happy",
-        "sad": "sad",
-        "angry": "angry",
-        "surprise": "surprised",
-        "disgust": "disgusted",
-        "fear": "afraid"}
+               "happy": "happy",
+               "sad": "sad",
+               "angry": "angry",
+               "surprise": "surprised",
+               "disgust": "disgusted",
+               "fear": "afraid"}
 
 try:
     while True:
-        avail, frame = feed.read() #avail --> if the webcam is available 
+        avail, frame = feed.read()  # avail --> if the webcam is available
         if not avail:
             break
         no_face = False
@@ -41,8 +41,10 @@ try:
             emotion_type = max(emotions, key=emotions.get)
             emotion_score = emotions[emotion_type]
             emotion_text = f"{emotion_type}: {emotion_score:.2f}"
-            cv2.putText(frame, emotion_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-        session.post('http://localhost:2319/set_emoji', data={'no_face': str(no_face), 'emoji': emo_map[emotion_type], 'emotion': emo_grammar[emotion_type], 'surety': f"{emotion_score:.2f}"}, timeout=0.1)
+            cv2.putText(frame, emotion_text, (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        session.post('http://localhost:2319/set_emoji', data={'no_face': str(
+            no_face), 'emoji': emo_map[emotion_type], 'emotion': emo_grammar[emotion_type], 'surety': f"{emotion_score:.2f}"}, timeout=0.1)
         # print(emo_map[emotion_type], emotion_type, no_face)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
