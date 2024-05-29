ba69bec983ff754c8b75ff4e2feeda7984b3e662
Dharmik Kaku <126943942+dharmik2319@users.noreply.github.com>
2024-05-29 16:33:19 +0530

    ran pylint and autopep8 on web_server.py

--- emotions-ai/AI/web_server.py
+++ emotions-ai/AI/web_server.py
@@ -1,31 +1,45 @@
-from flask import Flask, jsonify, redirect, render_template, request, g, url_for, json
+"""This module contains code to run the web server and REST API"""
+
+from typing import Dict
+from flask import Flask, render_template, request
 
 app: Flask = Flask(__name__)
-app.json.ensure_ascii=False # for sending utf-8/unicode characters (emojis). prolly needed, but i wouldnt touch it
-vars = {'emoji': '',
-        'emotion': '',
-        'surety': '',
-        'no_face': ''} # instead of using the problematic flask.g, (for global variables), a dict is 1000x better.
+global_variables: Dict[str, str] = {'emoji': '',
+                         # instead of using the problematic flask.g, (for global variables), a dict
+                                    'emotion': '',
+                                    'surety': '',  # is 1000x better.
+                                    'no_face': ''}
+
 
 @app.route('/')
 def home():
-    return render_template('index.html', emoji=vars['emoji'])
+    """Renders the main homepage (index.html) with the specified emoji"""
+    return render_template('index.html', emoji=global_variables['emoji'])
+
 
 @app.route('/set_emoji', methods=['POST'])
 def set_emoji():
-    vars['emoji'] = request.form.get('emoji')
-    vars['surety'] = request.form.get('surety')
-    vars['emotion'] = request.form.get('emotion')
-    vars['no_face'] = request.form.get('no_face')
-    if vars['no_face']=='True':
-        return f"<span class='emoji'>❔</span>\n<span class='text'> No faces found!</span>"
-    return f"<span class='emoji'>{vars['emoji']}</span>\n<span class='text'>{vars['surety']}% sure that you are <span class='emotion'>{vars['emotion']}</span>.</span>"
+    """POST /set_emoji endpoint to set the emoji and other data"""
+    global_variables['emoji'] = str(request.form.get('emoji'))
+    global_variables['surety'] = str(request.form.get('surety'))
+    global_variables['emotion'] = str(request.form.get('emotion'))
+    global_variables['no_face'] = str(request.form.get('no_face'))
+    if global_variables['no_face'] == 'True':
+        return "<span class='emoji'>❔</span>\n<span class='text'> No faces found!</span>"
+    return f"<span class='emoji'>{global_variables['emoji']}</span>\n<span class='text'>" \
+        "{global_variables['surety']}% sure that you are <span class='emotion'>" \
+        "{global_variables['emotion']}</span>.</span>"
+
 
 @app.route('/get_emoji', methods=['GET'])
 def get_emoji():
-    if vars['no_face']=='True':
-        return f"<span class='emoji'>❔</span>\n<span class='text'> No faces found!</span>"
-    return f"<span class='emoji'>{vars['emoji']}</span>\n<span class='text'>{vars['surety']}% sure that you are <span class='emotion'>{vars['emotion']}</span>.</span>"
+    """GET /get_emoji enpoint to fetch current emoji and other data"""
+    if global_variables['no_face'] == 'True':
+        return "<span class='emoji'>❔</span>\n<span class='text'> No faces found!</span>"
+    return f"<span class='emoji'>{global_variables['emoji']}</span>\n<span class='text'>" \
+        f"{global_variables['surety']}% sure that you are <span class='emotion'>" \
+        f"{global_variables['emotion']}</span>.</span>"
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(port=2319, debug=True)
